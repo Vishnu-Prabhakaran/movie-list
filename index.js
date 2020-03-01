21a9489765fe722ba71590ee14e0825ff3763100
Vishnu-Prabhakaran <vampire.wish@gmail.com>
2020-03-01 23:46:48 +1100

    Using Nightmare to get the poster image links

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const request = require('request-promise');
 const cheerio = require('cheerio');
+// Nightmare
+const Nightmare = require('nightmare');
+const nightmare = Nightmare({ show: true });
 
 async function scrapeTitlesRanksAndRatings() {
   const result = await request.get(
-    'https://www.imdb.com/chart/moviemeter/?ref_=nv_mv_250'
+    'https://www.imdb.com/chart/moviemeter/?ref_=nv_mv_mpm'
   );
   const $ = await cheerio.load(result);
   const movies = $('tr')
@@ -43,16 +46,36 @@ async function scrapePosterUrl(movies) {
         movie.posterUrl = 'https://www.imdb.com' + movieUrl;
         return movie;
       } catch (err) {
-        //console.log(err);
+        console.log(err);
       }
     })
   );
   return moviesWithPosterUrls;
 }
+// Get Poster
+async function scrapePosterImageUrl(movies) {
+  for (var i = 0; i < movies.length; i++) {
+    try {
+      const posterImageUrl = await nightmare
+        .goto(movies[i].posterUrl)
+        .evaluate(() =>
+          $(
+            '#photo-container > div > div:nth-child(3) > div > div.pswp__scroll-wrap > div.pswp__container > div:nth-child(2) > div > img:nth-child(2)'
+          ).attr('src')
+        );
+      movies[i].posterImageUrl = posterImageUrl;
+      console.log(movies[i]);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  return movies;
+}
 
 async function main() {
   let movies = await scrapeTitlesRanksAndRatings();
   movies = await scrapePosterUrl(movies);
+  movies = await scrapePosterImageUrl(movies);
   console.log(movies);
 }
 
