5a363e68b37852480283ad388b242a62f6c66a2b
Vishnu-Prabhakaran <vampire.wish@gmail.com>
2020-03-01 23:24:15 +1100

    basic scrape

--- index.js
+++ index.js
@@ -8,23 +8,52 @@ async function scrapeTitlesRanksAndRatings() {
   const $ = await cheerio.load(result);
   const movies = $('tr')
     .map((i, e) => {
-      const title = $(e)
-        .find('td.titleColumn > a')
-        .text();
-      const descriptionUrl =
-        'https://www.imdb.com' +
-        $(e)
+      try {
+        const title = $(e)
           .find('td.titleColumn > a')
-          .attr('href');
-      const imdbRating = $(e)
-        .find('td.ratingColumn.imdbRating')
-        .text()
-        .trim();
-      // Rank is the index
-      return { title, imdbRating, rank: i, descriptionUrl };
+          .text();
+
+        const descriptionUrl =
+          'https://www.imdb.com' +
+          $(e)
+            .find('td.titleColumn > a')
+            .attr('href');
+        const imdbRating = $(e)
+          .find('td.ratingColumn.imdbRating')
+          .text()
+          .trim();
+        // Rank is the index
+        return { title, imdbRating, rank: i, descriptionUrl };
+      } catch (err) {
+        console.log(err);
+      }
     })
     .get();
+  return movies;
+}
+
+async function scrapePosterUrl(movies) {
+  const moviesWithPosterUrls = await Promise.all(
+    movies.map(async movie => {
+      try {
+        const html = await request.get(movie.descriptionUrl);
+        const $ = await cheerio.load(html);
+
+        const movieUrl = $('div.poster > a').attr('href');
+        movie.posterUrl = 'https://www.imdb.com' + movieUrl;
+        return movie;
+      } catch (err) {
+        //console.log(err);
+      }
+    })
+  );
+  return moviesWithPosterUrls;
+}
+
+async function main() {
+  let movies = await scrapeTitlesRanksAndRatings();
+  movies = await scrapePosterUrl(movies);
   console.log(movies);
 }
 
-scrapeTitlesRanksAndRatings();
+main();
